87aae72fd566ccc53dfc846bf3c614c6cf9d1df6
Vito Gentile <gentilevito1076@gmail.com>
2021-02-05 13:44:02 +0100

    File Preparati

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*TRACCIA:
+Eseguire in Vue.js una chiamata ajax, generare 10 email e stamparle a schermo.
+La chiamata ajax vi ritornerà ogni volta un'email random.
+Endpoint: https://flynn.boolean.careers/exercises/api/random/mail*/
+
+new Vue({
+ el: '#root',
+
+});
+Vue.config.devtools = true;
