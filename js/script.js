7fd2bea44d1a123b9b87ab67aba534acdb35beb4
Vito Gentile <gentilevito1076@gmail.com>
2021-02-05 15:35:05 +0100

    Prova creazione Array email

--- js/script.js
+++ js/script.js
@@ -3,10 +3,36 @@ Eseguire in Vue.js una chiamata ajax, generare 10 email e stamparle a schermo.
 La chiamata ajax vi ritornerà ogni volta un'email random.
 Endpoint: https://flynn.boolean.careers/exercises/api/random/mail*/
 
+// new Vue({
+//   el: '#root',
+//   data: {
+//     emailRand: ''
+//   },
+//   mounted() {
+//     //Definisco una const 'self' per bypassare il problema di scope interno alla FX (per il response) che farò eseguire per 10 volte attraverso il ciclo For che andrò a scrivere
+//     const self = this;
+//     //Verifica in console che l'app sia collegata
+//     console.log('app caricata');
+//     //Ciclo per 10 volte la get su axios all'url specificato
+//     for (var i = 0; i < 10; i++) {
+//       axios.get(' https://flynn.boolean.careers/exercises/api/random/mail')
+//       .then(function(resp) {
+//         //Se la chiamata ha successo, in termini di risposta, allora evadi il risultato in console, ad ogni iterazione.
+//         console.log('email: ', resp.data);
+//         //Col .response risalgo esattamente al valore dell'email interna all'object restituito
+//         self.emailRand += resp.data.response;
+//       });
+//     }
+//   }
+//
+// });
+// Vue.config.devtools = true;
+// -------
 new Vue({
   el: '#root',
   data: {
-    emailRand: ''
+    emailRand: '',
+    emailList: []
   },
   mounted() {
     //Definisco una const 'self' per bypassare il problema di scope interno alla FX (per il response) che farò eseguire per 10 volte attraverso il ciclo For che andrò a scrivere
@@ -16,11 +42,14 @@ new Vue({
     //Ciclo per 10 volte la get su axios all'url specificato
     for (var i = 0; i < 10; i++) {
       axios.get(' https://flynn.boolean.careers/exercises/api/random/mail')
+
       .then(function(resp) {
         //Se la chiamata ha successo, in termini di risposta, allora evadi il risultato in console, ad ogni iterazione.
         console.log('email: ', resp.data);
         //Col .response risalgo esattamente al valore dell'email interna all'object restituito
-        self.emailRand += resp.data.response;
+        self.emailRand = resp.data.response;
+        self.emailList.push(self.emailRand);
+        console.log('Ecco l\'array delle email: ' +  self.emailList);
       });
     }
   }
